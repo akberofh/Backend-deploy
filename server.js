87322b2c6540db150d4246e1c01c88209b9511c5
akberofh <sd85bejal@code.edu.az>
2024-10-16 03:04:57 +0400

    fuhhhg

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use(cors({
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5000;
 
 connectDB();
 
