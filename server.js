b342dcc82e051eef2977950b2cf0ee0e5c0e7054
akberofh <sd85bejal@code.edu.az>
2024-10-16 02:32:10 +0400

    commitforst

--- server.js
+++ server.js
@@ -5,18 +5,11 @@ import noteRoutes from './routes/noteRoutes.js';
 import connectDB from './config/db.js';
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
-import { Server } from 'socket.io';
 import http from 'http';
 dotenv.config();
 const app = express();
 const server = http.createServer(app);
-const io = new Server(server, {
-    cors: {
-        origin: 'http://localhost:3000',
-        methods: ['GET', 'POST'],
-        credentials: true,
-    },
-});
+
 
 app.use(express.json());
 app.use(cors({
@@ -33,21 +26,7 @@ connectDB();
 app.use('/api/users', userRoutes);
 app.use('/api/notes', noteRoutes);
 
-// Socket.IO bağlantısını dinleme
-io.on('connection', (socket) => {
-    console.log('Yeni bir kullanıcı bağlandı');
-
-    socket.on('chat message', (msg) => {
-        console.log('Mesaj alındı: ' + msg);
-        // Mesajı tüm istemcilere gönder
-        io.emit('chat message', msg);
-    });
-
-    socket.on('disconnect', () => {
-        console.log('Kullanıcı ayrıldı');
-    });
-});
 
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
-io.listen(server);
+
